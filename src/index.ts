4a99c1fe02c8b8024cdb49ff6565f9ff0dd22fdc
Dani Verhaar <dani@bluenotion.nl>
2023-02-23 11:49:03 +0100

    Initial

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,65 @@
-/* eslint-disable */
-
 import {env} from "../environment";
-import {iwa} from "./scraper";
+import {getRecentPosts} from "./scraper";
+import fetch from "node-fetch";
+import fetchList from "../fetchList.json";
 
-async function fetch() {
-    const responseIwa = await iwa({
-        headers: {
-            cookie: env.IG_COOKIE,
-            "user-agent": env.USER_AGENT,
-            "x-ig-app-id": env.IG_APP_ID,
-        },
-        base64images: false,
-        maxImages: 1,
-        pretty: true,
-        time: 3600,
-        id: "monkeloidtv",
-    });
-
-    console.log({responseIwa});
+const headers = {
+	cookie: env.IG_COOKIE,
+	"user-agent": env.USER_AGENT,
+	"x-ig-app-id": env.IG_APP_ID,
 }
 
-fetch();
+const cachedIds: string[] = [];
+
+async function main() {
+	let initial = false;
+	for (const item of fetchList.fetchList) {
+		const response = await getRecentPosts({
+			headers,
+			maxImages: 1,
+			pretty: true,
+			id: item.id,
+		});
+
+		if (cachedIds.length < 1) initial = true;
+		if (response.length < 1) return;
+		if (cachedIds.includes(response[0].id)) return;
+
+		cachedIds.push(response[0].id);
+		console.log(response);
+		if (!initial) sendToDiscord(item.name, item.avatar, response[0])
+	}
+}
+
+async function sendToDiscord(name: string, avatar: string, post: any) {
+	const msg: any = {
+		avatar_url: avatar,
+		username: name,
+		embeds: [
+			{
+				description: replaceStrings(post.text),
+				image: { url: post.imageUrl }
+			}
+		]
+	}
+
+	fetch(env.WEBHOOK_URL, {
+		method: "POST",
+		body: JSON.stringify(msg),
+		headers: {
+			"Content-Type": "application/json",
+		}
+	});
+}
+
+function replaceStrings(text: string): string {
+	const newText = text.replace("@kristelverbeke", "<@320273356501024769>")
+		.replace("@genethomasmusic", "<@224444822109290496>")
+		.replace("@lilythomas09", "<@55035219484344320>")
+		.replace("@nanouthomas01", "<@656861522718621717>");
+
+	return newText;
+}
+
+main();
+setInterval(main, 5000);
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,84 +1,127 @@
 /* eslint-disable */
 const request = require("request");
-const fs = require("fs");
 
-export function iwa({id, maxImages, file, pretty, time, base64images, headers}: any) {
-    if (!id) id = "therock";
+export function getRecentPosts({
+    id,
+    maxImages,
+    pretty,
+    headers
+}: any): any {
+
+    if (!id) id = 'therock';
     if (!maxImages || Number.isNaN(maxImages)) maxImages = 12;
     if (!pretty) pretty = false;
-    if (Number.isNaN(time)) time = 3600;
-    if (!base64images) base64images = false;
     if (!headers) headers = {};
 
     const options = {
         url: `https://i.instagram.com/api/v1/users/web_profile_info/?username=${id}`,
-        headers,
+        headers
     };
 
-    function requestAsync(options: any) {
-        let result: any = [];
-        return new Promise(function (resolve) {
-            request(options, async (error: any, res: any, body: any) => {
-                console.log(body);
-                if (!error && res.statusCode == 200) {
-                    let json = null;
-                    try {
-                        json = JSON.parse(body);
-                    } catch (e) {
-                        console.log(e);
-                    }
-                    console.log(json);
-                    if (!json) return;
-                    const items = json?.data?.user?.edge_owner_to_timeline_media?.edges || [];
-                    const filteredItems = items?.filter((el: any, index: any) => {
-                        return !maxImages ? el : index < maxImages;
-                    });
-                    const mappedItems = await Promise.all(
-                        filteredItems.map(async (el: any) => {
-                            let obj = {
-                                id: el["node"]["id"],
-                                time: el["node"]["taken_at_timestamp"],
-                                imageUrl: el["node"]["display_url"],
-                                likes: el["node"]["edge_liked_by"]["count"],
-                                comments: el["node"]["edge_media_to_comment"]["count"],
-                                link: "https://www.instagram.com/p/" + el["node"]["shortcode"] + "/",
-                                text: el["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"],
-                            };
-
-                            return obj;
-                        }),
-                    );
-                    result = mappedItems;
-                }
-
-                resolve(result);
-            });
-        });
-    }
-
-    return init({file, time, callback: requestAsync, options});
+	let result: any = []
+	return new Promise(function (resolve) {
+		request(options, async (error: any, res: any, body: any) => {
+			if (!error && res.statusCode == 200) {
+				let json = null
+				try { json = JSON.parse(body); } catch (e) { console.log(e) }
+				const items = json?.data?.user?.edge_owner_to_timeline_media?.edges || [];
+				const filteredItems = items.filter((el: any, index: any) => { return !maxImages ? el : index < maxImages });
+				const mappedItems = await Promise.all(filteredItems.map(async (post: any) => {
+					const obj = {
+						id: post.node.id,
+						time: post.node.taken_at_timestamp,
+						imageUrl: post.node.display_url,
+						likes: post.node.edge_media_to_comment.count,
+						comments: post.node.edge_media_to_comment.count,
+						link: 'https://www.instagram.com/p/' + post.node.shortcode + '/',
+						text: post?.node.edge_media_to_caption?.edges[0]?.node?.text
+					}
+					return obj;
+				}));
+				result = mappedItems;
+			}
+			resolve(result);
+		});
+	});
 }
 
-function init({file, time, callback, options}: any) {
-    return new Promise(function (resolve) {
-        let result: any = [];
-        try {
-            fs.stat(file, async (err: any, stats: any) => {
-                if (err) {
-                    result = await callback(options);
-                    resolve(result);
-                    return;
-                }
-                const date = new Date();
-                const unixTimestampNow = Math.floor(date.getTime() / 1000);
-                const unixTimestamp = Math.floor(stats.mtime.getTime() / 1000);
-                // console.log(unixTimestampNow - unixTimestamp >= time);
-                if (unixTimestampNow - unixTimestamp >= time) result = await callback(options);
-                else result = JSON.parse(fs.readFileSync(file, "utf8") || []);
-                resolve(result);
-            });
-        } catch (err) {
-            resolve(result);
-        }
-    });
-}
+// maybe for later
+// export function iwaId({
+//     id,
+//     pretty,
+//     headers
+// }: any) {
+
+//     if (!id) id = '2890411760684296309';
+//     if (!pretty) pretty = false;
+//     if (!headers) headers = {};
+
+//     const options = {
+//         url: `https://i.instagram.com/api/v1/media/${id}/info/`,
+//         headers
+//     };
+
+// 	let result: any = []
+// 	return new Promise(function (resolve) {
+// 		request(options, async (error: any, res: any, body: any) => {
+// 			if (!error && res.statusCode == 200) {
+// 				let json = null
+// 				try { json = JSON.parse(body); } catch (e) { }
+
+// 				const item = json?.items?.[0] || false;
+// 				const image =
+// 					json?.['items']?.[0]?.['image_versions2']?.['candidates']?.[0]?.['url'] ?
+// 						json['items'][0]['image_versions2']['candidates'][0] :
+// 						json?.['items']?.[0]?.['carousel_media']?.[0]?.['image_versions2']?.['candidates']?.[0]?.['url'] ?
+// 							json['items'][0]['carousel_media'][0]['image_versions2']['candidates'][0] :
+// 							false;
+
+// 				let mappedItems: any = [];
+
+// 				if (item && image) {
+// 					let comments: any= [];
+// 					if (item['comments'] && item['comments'].length) {
+// 						for (let i = 0; i < item['comments'].length; i++) {
+// 							let value = item['comments']?.i;
+// 							if (value) {
+// 								comments.push({
+// 									time: value['created_at_utc'],
+// 									text: value['text'],
+// 									user: {
+// 										username: value['user']['username'],
+// 										fullName: value['user']['full_name'],
+// 										imageUrl: value['user']['profile_pic_url']
+// 									}
+// 								});
+// 							}
+// 						}
+// 					}
+
+// 					mappedItems = await Promise.all([item].map(async (el) => {
+// 						let obj = {
+// 							id,
+// 							width: image['width'],
+// 							height: image['height'],
+// 							imageUrl: image['url'],
+// 							time: el['taken_at'],
+// 							topLikers: el['top_likers'],
+// 							likes: el['like_count'],
+// 							commentCount: el['comment_count'] || 0,
+// 							comments,
+// 							link: `https://www.instagram.com/p/${el['code']}/`,
+// 							text: el?.['caption']?.['text'],
+// 							user: {
+// 								username: el?.['user']?.['username'],
+// 								fullName: el?.['user']?.['full_name'],
+// 								imageUrl: el?.['user']?.['profile_pic_url']
+// 							}
+// 						}
+// 						return obj
+// 					}));
+// 				}
+// 				result = mappedItems.length ? mappedItems[0] : {};
+// 			}
+// 			resolve(result);
+// 		});
+// 	});
+// }
